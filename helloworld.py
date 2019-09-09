7f9d1a5340285f58ede2e093e27a98c3a74e6b09
abruf <abruf@umich.edu>
2019-09-09 13:12:48 -0400

    Added helloworld.py with included elements

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from random import random
+
+print "Alex"
+numberOne = int(round(random() * 100))
+numberTwo = int(round(random() * 100))
+print numberOne
+print numberTwo
+print "Sum = " + str(numberOne + numberTwo)
+print "Average = " + str(float(numberOne + numberTwo)/2)
